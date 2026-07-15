46ef5456506862f126711c2525052beab47a5f86
hyundonghoon <bunnyrabbit@kaist.ac.kr>
2026-07-15 21:18:15 +0900

    feat: bind CI portable format and null flavor rules

--- crates/libs/validator/src/portable_bindings/c.rs
+++ crates/libs/validator/src/portable_bindings/c.rs
@@ -3,10 +3,37 @@ use super::{PortableFieldBinding, PortableValueType};
 pub(super) const BINDINGS: &[PortableFieldBinding] = &[
 	PortableFieldBinding {
 		section: "CI",
+		frontend_path: "safetyReportIdentification.transmissionDate",
+		request_path: "transmissionDate",
+		value_type: PortableValueType::String,
+		rule_codes: &["ICH.C.1.2.ALLOWED.VALUE"],
+		null_flavor_path: None,
+	},
+	PortableFieldBinding {
+		section: "CI",
 		frontend_path: "safetyReportIdentification.reportType",
 		request_path: "reportType",
 		value_type: PortableValueType::String,
 		rule_codes: &["ICH.C.1.3.LENGTH.MAX", "ICH.C.1.3.ALLOWED.VALUE"],
+		null_flavor_path: None,
+	},
+	PortableFieldBinding {
+		section: "CI",
+		frontend_path: "safetyReportIdentification.fulfilExpeditedCriteria",
+		request_path: "fulfilExpeditedCriteria",
+		value_type: PortableValueType::Boolean,
+		rule_codes: &["ICH.C.1.7.ALLOWED.VALUE"],
+		null_flavor_path: Some(
+			"safetyReportIdentification.fulfilExpeditedCriteriaNullFlavor",
+		),
+	},
+	PortableFieldBinding {
+		section: "CI",
+		frontend_path:
+			"safetyReportIdentification.fulfilExpeditedCriteriaNullFlavor",
+		request_path: "fulfilExpeditedCriteriaNullFlavor",
+		value_type: PortableValueType::String,
+		rule_codes: &["ICH.C.1.7.NULLFLAVOR.ALLOWED"],
 		null_flavor_path: None,
 	},
 	PortableFieldBinding {
